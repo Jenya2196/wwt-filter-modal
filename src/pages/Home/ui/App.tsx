8c71683a73493304b43d32a7265df06b1c5a9026
Genya_2196 <genya212121@gmail.com>
2025-09-13 00:36:14 +0300

    refactor(App): restructure header and main sections for improved layout

--- src/pages/Home/ui/App.tsx
+++ src/pages/Home/ui/App.tsx
@@ -10,17 +10,17 @@ export const App = () => {
 
 	return (
 		<>
-			<section className="w-full h-dvh flex flex-col items-center gap-3">
-				<h1 className="text-6xl text-gray-600">
-					WinWinTravel frontend test task
-				</h1>
-				<Button onClick={() => setOpen(true)}>Open Filters</Button>
+			<header className="w-full flex flex-col items-center gap-3">
+				<h1 className="text-6xl text-gray-600">WinWinTravel</h1>
+				<Button onClick={() => setOpen(true)}>Filters</Button>
+			</header>
 
-				{/* Отображение выбранных фильтров для дебага */}
-				<div className="w-3/4 max-w-3xl p-4 bg-gray-100 rounded text-sm text-gray-700 overflow-auto">
+			<main className="flex justify-center p-4 ">
+				<section className="max-w-3xl p-4 bg-gray-100 rounded text-sm text-gray-700 overflow-auto">
+					<h2>Selected Filters</h2>
 					<pre>{JSON.stringify(filter, null, 2)}</pre>
-				</div>
-			</section>
+				</section>
+			</main>
 
 			<FilterModal
 				onClose={() => setOpen(false)}
